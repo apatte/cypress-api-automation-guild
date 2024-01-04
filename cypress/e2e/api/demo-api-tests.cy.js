5ddb6b5d89f96ba72141d08512213aac6c77c741
Anna Patterson <64214550+apatte@users.noreply.github.com>
2024-01-04 15:08:31 -0500

    create demo spec

---
+++ cypress/e2e/api/demo-api-tests.cy.js
@@ -0,0 +1,5 @@
+describe("Pet API Tests", () => {
+  it("POST - should create a new pet", () => {});
+  it("GET - should get a pet by ID", () => {});
+  it("DELETE - should delete the created pet", () => {});
+});
